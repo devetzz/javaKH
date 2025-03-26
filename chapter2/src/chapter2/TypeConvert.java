eb12171e01ac41fe0d91769e54aab0b486a5e3e6
user1 <user1@DESKTOP-CDP4U23>
2025-03-26 15:36:16 +0900

    2025.03.26 15:36 commit

---
+++ chapter2/src/chapter2/TypeConvert.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+public class TypeConvert {
+
+	public static void main(String[] args) {
+		int i = 0;
+		double f = 0.0;
+		
+		// 피연산자가 정수이므로 정수 연산이 된다.
+		f = 1 / 5;
+		System.out.println(f);
+		//형변환 연산자가 먼저 1을 소수로 바꾸고 1.0 / 5는 피연산자중 하나가 double이라서 자동 형변환되어 연산된다.
+		f = (double)1 / 5;
+		System.out.println(f);
+		// 평변환 연산자가 1.7과 1.8 모두 1로 바꾼 후 연산하여 1+1=2가 된다.
+		i = (int)1.7 / (int)1.8;
+		System.out.println(i);
+	}
+
+}
