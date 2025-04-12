58a0639e66f3147dbc51b260e34ac973a636cd10
parkkangho <etzz@naver.com>
2025-04-12 17:29:29 +0900

    2025.04.12_1

---
+++ practice/src/selftraning/Test.java
@@ -0,0 +1,22 @@
+package selftraning;
+
+import java.text.DecimalFormat;
+
+public class Test {
+
+	public static void main(String[] args) {
+		DecimalFormat formatter = new DecimalFormat("###,###");
+		int price1 = 123;
+		int price2 = 1234;
+		int price3 = 12345;
+		int price4 = 123456;
+		int price5 = 1234567;
+		
+		System.out.println(formatter.format(price1));
+		System.out.println(formatter.format(price2));
+		System.out.println(formatter.format(price3));
+		System.out.println(formatter.format(price4));
+		System.out.println(formatter.format(price5));
+	}
+
+}
