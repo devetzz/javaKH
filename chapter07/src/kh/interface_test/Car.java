a4b7701ac3b3303d83424be2af7b8fc9ddb5875f
devetzz <etzz@naver.com>
2025-04-15 00:44:26 +0900

    update

--- chapter07/src/kh/interface_test/Car.java
+++ chapter07/src/kh/interface_test/Car.java
@@ -7,12 +7,12 @@ public class Car implements Bomb {
 		super();
 	}
 
-	@Override
+	
 	public void explodeBomb() {
 		System.out.println("차가 폭발했습니다.");
 	}
 
-	@Override
+	
 	public void printBrand() {
 		System.out.println("차의 디폴트 메소드");
 	}
